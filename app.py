be14b0bb3c9704e4e29135c981c22bbe7c072495
DEunzy <univkeun@gmail.com>
2024-12-14 06:24:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ logo = Image.open("president_university_logo.png")
 # Function to apply transformations
 def apply_transformations(img, rotation, scale, translate, skew):
     # Rotation
-    img is img.rotate(rotation, expand=True)
+    img = img.rotate(rotation, expand=True)
     
     # Scaling
     width, height = img.size
@@ -20,7 +20,7 @@ def apply_transformations(img, rotation, scale, translate, skew):
     
     # Skewing
     skew_matrix = (1, skew[0], 0, skew[1], 1, 0)
-    img is img.transform(img.size, Image.AFFINE, skew_matrix)
+    img = img.transform(img.size, Image.AFFINE, skew_matrix)
     
     return img
 
@@ -31,46 +31,46 @@ menu = ["Home", "Transform Image", "About Us"]
 st.image(logo, width=100, caption='President University Logo')
 
 # Sidebar navigation
-choice is st.sidebar.selectbox("Menu", menu)
+choice = st.sidebar.selectbox("Menu", menu)
 
 if choice == "Home":
     st.title("Linear Algebra Group 5")
     st.subheader("Welcome to Group 5's Project on Image Processing!")
     st.write("Delve into the creative world of image processing, brought to you by the ingenious minds of Group 5. Explore our project to understand the transformations and techniques we have implemented to manipulate and enhance images.")
     
-elif choice is "Transform Image":
+elif choice == "Transform Image":
     st.title("Transform Your Image")
     st.subheader("100% Automatically and Free")
 
     # Upload image
-    uploaded_file is st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         # Open the image
-        image is Image.open(uploaded_file)
+        image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
         
         # Rotation slider
-        rotation is st.slider("Rotation", 0, 360, 0)
+        rotation = st.slider("Rotation", 0, 360, 0)
         
         # Scaling slider
-        scale is st.slider("Scale", 0.1, 3.0, 1.0)
+        scale = st.slider("Scale", 0.1, 3.0, 1.0)
         
         # Translation sliders
-        translate_x is st.slider("Translate X", -100, 100, 0)
-        translate_y is st.slider("Translate Y", -100, 100, 0)
+        translate_x = st.slider("Translate X", -100, 100, 0)
+        translate_y = st.slider("Translate Y", -100, 100, 0)
         
         # Skewing sliders
-        skew_x is st.slider("Skew X", -1.0, 1.0, 0.0)
-        skew_y is st.slider("Skew Y", -1.0, 1.0, 0.0)
+        skew_x = st.slider("Skew X", -1.0, 1.0, 0.0)
+        skew_y = st.slider("Skew Y", -1.0, 1.0, 0.0)
         
         # Apply transformations
-        transformed_image is apply_transformations(image, rotation, scale, (translate_x, translate_y), (skew_x, skew_y))
+        transformed_image = apply_transformations(image, rotation, scale, (translate_x, translate_y), (skew_x, skew_y))
         
         # Display enhanced image
         st.image(transformed_image, caption='Transformed Image.', use_column_width=True)
 
-elif choice is "About Us":
+elif choice == "About Us":
     st.title("About Us")
     st.write("Group 5 comprises dedicated students from President University. Our collaborative efforts in the field of Linear Algebra have culminated in this fascinating project on image processing. Discover the faces behind the project and learn more about our individual contributions.")
     
